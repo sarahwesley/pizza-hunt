2022e717ba4a8a00d2c9ae6a208e85de786329a4
sarahwesley <92330391+sarahwesley@users.noreply.github.com>
2022-02-26 16:07:34 -0500

    comment and reply model added

---
+++ models/Comment.js
@@ -0,0 +1,62 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReplySchema = new Schema( 
+    {
+    replyID: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    replyBody: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    writtenBy: {
+        type: String,
+        require: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    }
+},
+{
+    toJSON: {
+      getters: true
+    }
+  }
+
+);
+
+const CommentSchema = new Schema ({
+    writtenBy: {
+        type: String,
+        required: true
+    },
+    commentBody: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    replies: [ReplySchema]
+},
+{
+    toJSON: {
+        virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+
+);
+CommentSchema.virtual('replyCount').get(function() {
+    return this.replies.length;
+});
+
+const Comment = model('Comment', CommentSchema);
+module.exports = Comment;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
+const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
-//add prefiz of /pizzas to routes created in pizza-routes.js
-router.use('./pizzas', pizzaRoutes);
+router.use('/comments', commentRoutes);
+router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;
